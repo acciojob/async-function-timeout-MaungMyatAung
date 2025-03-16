d45c4451756c3016dc470f11f128e30586fc1b5d
MaungMyatAung <85542468+MaungMyatAung@users.noreply.github.com>
2025-03-16 13:24:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+
+let output = document.getElementById("output");
+let text = document.getElementById("text").value;
+let delay = document.getElementById("delay").value;
+
+document.getElementById("btn").addEventListner("click", async ()=>{
+	let p = new Promise((resolve, reject)=>{
+		setTimeout(()=>{
+			resolve();
+		},delay.value)
+	})
+	let res = await p;
+	output innerText = text.value;
+	
+})
+
+
+
+
+
+
